e336aadc61fcff0bf6475730b85872f57ea3a359
Aryan Jain <aryan.jain.csbs22@ggits.net>
2025-02-16 20:16:33 +0530

    chat

--- app/(chat)/layout.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { ReactNode } from "react";
-
-export default function Layout({ children }: { children: ReactNode }) {
-    return <div className="bg-black text-white min-h-screen">{children}</div>;
-}
